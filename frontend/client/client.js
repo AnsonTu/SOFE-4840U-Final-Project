dd8d9bbf80dd4ac3d8c0bbc0357ae283203c4449
Anson Tu <anson9137@gmail.com>
2023-03-15 16:00:11 -0400

    Add error handling to auth forms

--- frontend/client/client.js
+++ frontend/client/client.js
@@ -2,7 +2,16 @@
 const submitCredentials = () => {
   const emailAddress = document.getElementById("emailField");
   const password = document.getElementById("passwordField");
-  const submitButton = document.getElementById("submitCredentialsButton");
+  const submitButton = document.getElementById("submitCredentialsBtn");
+  const statusText = document.getElementById("statusText");
+
+  if (emailAddress.value === "" || password.value === "") {
+    statusText.innerHTML = "Missing email address or password";
+  } else {
+    statusText.innerHTML = "";
+    submitButton.textContent = "Credentials submitted";
+    submitButton.disabled = true;
+  }
 };
 
 // Submit an order
