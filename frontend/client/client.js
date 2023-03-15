217338ba53c4e5e0e9241db6d94c5f40f572b386
Anson Tu <anson9137@gmail.com>
2023-03-15 15:20:43 -0400

    Add forms to allow different users to authenticate

--- frontend/client/client.js
+++ frontend/client/client.js
@@ -1,3 +1,10 @@
+// Send the client's email and password for authentication
+const submitCredentials = () => {
+  const emailAddress = document.getElementById("emailField");
+  const password = document.getElementById("passwordField");
+  const submitButton = document.getElementById("submitCredentialsButton");
+};
+
 // Submit an order
 const submitOrder = () => {
   const item = document.getElementById("items").value;
