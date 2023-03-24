ff621b2b949f799d527b59a4c16ad7869a4e3912
OwenMussy99 <48837649+OwenMussy99@users.noreply.github.com>
2023-03-24 18:39:11 -0400

    Cleaning outdated

--- backend/user_email_info.py
+++
@@ -1,9 +0,0 @@
-# Email info for the different depts.
-super_gmail = 'supervisorsecproj@gmail.com'
-super_pass = 'supersecproj123'
-
-orderdept_gmail = 'orderdeptsecproj@gmail.com'
-orderdept_pass = 'orderdeptsecproj123'
-
-purch_gmail = 'purchasecentresecproj@gmail.com'
-purch_pass = 'purchasecentresecproj123'
